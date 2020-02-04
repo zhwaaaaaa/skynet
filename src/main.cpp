47d7f7161764f05fda8f063d13696aaa4137f03f
zhwaaaaaa <852466752@qq.com>
2020-02-05 00:42:17 +0800

    listen two port

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,16 @@
 using namespace sn;
 
 int main() {
+    Reactor reactor1;
+    TcpListener<ClientAppHandler> listener1(EndPoint(IP_ANY, 9999));
+    reactor1.addLoopable(listener1);
+    reactor1.start(true);
 
-    Reactor reactor;
 
-    TcpListener<ClientAppHandler> listener(EndPoint(IP_ANY, 9999));
-    reactor.addLoopable(listener);
-    reactor.start();
-
+    Reactor reactor2;
+    TcpListener<ClientAppHandler> listener2(EndPoint(IP_ANY, 9998));
+    reactor2.addLoopable(listener2);
+    reactor2.start();
     return 0;
 }
+
